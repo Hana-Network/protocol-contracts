f21cea6362f905534036204858b26d514407927f
coopereve <evlcooper0322@gmail.com>
2024-04-25 17:21:04 +0900

    Update tool scripts

--- scripts/tools/set-zeta-token-addresses.ts
+++ scripts/tools/set-zeta-token-addresses.ts
@@ -1,9 +1,9 @@
 import { ethers, network } from "hardhat";
 import { getAddress, isProtocolNetworkName } from "lib";
 
-import { ZetaNonEth__factory as ZetaNonEthFactory } from "../../typechain-types";
+import { HanaNonEth__factory as HanaNonEthFactory } from "../../typechain-types";
 
-export async function setZetaAddresses(connectorAddress: string, zetaTokenAddress: string) {
+export async function setHanaAddresses(connectorAddress: string, hanaTokenAddress: string) {
   const [owner] = await ethers.getSigners();
 
   if (!isProtocolNetworkName(network.name)) {
@@ -14,18 +14,18 @@ export async function setZetaAddresses(connectorAddress: string, zetaTokenAddres
   const tssUpdaterAddress = getAddress("tssUpdater", network.name);
 
   if (owner.address !== tssAddress && owner.address !== tssUpdaterAddress) {
-    console.log("Only TSS or TSS Updater can set Zeta addresses.");
+    console.log("Only TSS or TSS Updater can set Hana addresses.");
     console.log("Please execute this step with a valid account.");
     return;
   }
 
   const [tssSigner] = await ethers.getSigners();
 
-  const contract = ZetaNonEthFactory.connect(zetaTokenAddress, tssSigner);
+  const contract = HanaNonEthFactory.connect(hanaTokenAddress, tssSigner);
 
   console.log("Updating");
   console.log("connectorAddress", connectorAddress);
-  console.log("zetaTokenAddress", zetaTokenAddress);
+  console.log("hanaTokenAddress", hanaTokenAddress);
   const tx = await contract.updateTssAndConnectorAddresses(tssAddress, connectorAddress);
   await tx.wait();
   console.log("Updated");
