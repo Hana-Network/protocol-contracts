949a422330e671d758b6f8901e8d4453538ad1fa
coopereve <evlcooper0322@gmail.com>
2024-06-23 19:57:28 +0900

    Remove noneed file

--- contracts/evm/hoge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in *Zeta*.sol; do
-    if [[ -f "$file" ]]; then
-        new_file="${file/Zeta/Hana}"
-        mv "$file" "$new_file"
-        echo "Renamed $file to $new_file"
-    fi
-done
